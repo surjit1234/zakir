b4888ca835225bedc6adb836cb287c87ba8dece7
surjit1234 <sujirt@gmail.com>
2022-08-11 14:14:48 +0530

    background change

--- src/App.js
+++ src/App.js
@@ -1,38 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState } from "react";
-import validator from 'validator';
-import Button from '@mui/material/Button';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import FirstPage from './FirstPage';
-import SecondPage from './SecondPage';
-import FinalPage from './FinalPage';
-function App() {
-  const [emailError, setEmailError] = useState('')
-  const validateEmail = (e) => {
-    var email = e.target.value
-  
-    if (validator.isEmail(email)) {
-      setEmailError('')
-    } else {
-      setEmailError('Enter valid Email!')
-    }
-  }
-  
-  return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<FirstPage />} />
-      <Route path="verifyopt" element={<SecondPage/>}/>  
-      <Route path="finalpage" element={<FinalPage/>}/>      
-    </Routes>
-  </BrowserRouter>
-  );
-}
+import React from 'react';
 
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
+
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
+    </div>
+    {/* <Brand /> */}
+    <WhatGPT3 />
+    {/* <Features />
+    <Possibility />
+    <CTA />
+    <Blog /> */}
+    <Footer />
+  </div>
+);
 
 export default App;
